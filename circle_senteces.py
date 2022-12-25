3aac53bbb11f288d289f45ece7184535ddef0640
Yasin Ermusmula <yasin.ermusmula1@gmail.com>
2022-12-26 00:30:57 +0300

    initial commit

---
+++ circle_senteces.py
@@ -0,0 +1,16 @@
+# my_list = "leetcode exercises sound delightful"
+
+
+def string(sentence):
+    words = sentence.split(" ")
+
+    for i in range(len(words)):
+        last_char = words[i][-1]
+        first_char = words[(i + 1) % len(words)][0]
+
+        if last_char != first_char:
+            return False
+    return True
+
+
+print(string("leetcode exercises sound aelightful"))
